03c33164157a89fb23d6b5bec08bae6e5993c7a1
shahin <shahinict09@gmail.com>
2018-08-18 04:33:24 +0600

    Config Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = '/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -448,7 +448,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
@@ -525,5 +525,7 @@ $config['proxy_ips'] = '';
 if (ENVIRONMENT == "development") {
 	$config['base_url'] = '/ciblog/';
 } else {
-	$config['base_url'] = '/';
+	$config['base_url'] = '/ciblog/';
 }
+$config['index_page'] = '';
+$config['csrf_protection'] = TRUE;
