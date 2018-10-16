804f3c052a0d70d1092ae028ce34e9a07be0a2e5
Shahin PBL <shahinict09@gmail.com>
2018-10-16 16:17:45 +0600

    front end design fixed

--- application/views/blog/featuredPost.php
+++ application/views/blog/featuredPost.php
@@ -4,15 +4,13 @@ $count = count($items);
 $slideItems = array_splice($items, 0, 3);
 ?>
 
-<div id="carouselExampleIndicators" class="carousel skui-carousel slide carousel-fade" data-ride="carousel">
-	<div class="carousel-inner">
+<div id="carouselExampleIndicators" class="carousel pb-3 skui-carousel slide carousel-fade" data-ride="carousel">
+	<div class="carousel-inner" style="height: 400px;">
 		<?php foreach ($slideItems as $i=>$post) {?>
 			<div class="carousel-item <?php echo ($i == 0 ? "active":"")?> jumbotron p-3 p-md-5 text-white rounded bg-dark h-100">
-				<div class="col-md-6 px-0">
-					<h1 class="font-italic"><?php echo $post["name"]?></h1>
-					<p class="lead my-3 overflow-hide"><?php echo $post["summary"]?></p>
-					<p class="lead mb-0"><a href="<?php echo base_url("post/".$post["id"])?>" class="text-white font-weight-bold">Continue reading...</a></p>
-				</div>
+				<h1 class="font-italic h-25"><?php echo $post["name"]?></h1>
+				<p class="lead my-3 overflow-hide h-50"><?php echo $post["summary"]?></p>
+				<p class="lead mb-0 float-right h-25"><a href="<?php echo base_url('post/'.$post['id'])?>" class="text-white font-weight-bold">Continue reading...</a></p>
 			</div>
 		<?php }?>
 	</div>
@@ -42,7 +40,9 @@ foreach($items as $i=>$post) {
 				<p class="card-text overflow-hide mb-auto"><?php echo $post["summary"]?></p>
 				<a href="<?php echo base_url("post/".$post["id"])?>">Continue reading...</a>
 			</div>
-			<img class="card-img-right flex-auto d-none d-lg-block" src="<?php echo(base_url($post["thumb"] ? $post["thumb"]: AppUtil::NO_IMAGE_THUMB))?>" style="max-width: 200px; max-height: 200px">
+			<div class="card-img-right flex-auto d-none d-lg-block overflow-hide" style="max-height: 200px; max-width: 200px;">
+				<img class="img-fluid" src="<?php echo(base_url($post["thumb"] ? $post["thumb"]: AppUtil::NO_IMAGE_THUMB))?>">
+			</div>
 		</div>
 	</div>
 <?php } ?>
